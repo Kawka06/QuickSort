8a699521c88863fbdfa5061c8e2f41cca4cb4ad2
Kawka-6 <kawka.mateusz1990606@gmail.com>
2020-05-22 18:29:52 +0200

    Dodaj pliki projektów.

---
+++ QuickSort.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std; 
+
+void Sort(int, int);
+
+int tab[a] = { 5, 60, 10, 40, 3 };
+
+int main()
+{
+
+}
+
+
+void Sort(int left, int right)
+{
+	int i, j, pivot;
+	
+	i = (left + right) / 2;
+}
+
+
